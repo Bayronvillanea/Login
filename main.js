88c9161be119666f6d14a3d38ced851172ae73fa
bayron-Bala <bayronvillaneahidalgo.30@gmail.com>
2023-02-26 14:35:49 -0600

    cambios de alertas

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ boton.addEventListener('click',(e) => {
     e.preventDefault()
    let usuario = document.querySelector('.usuario').value
    let password =  document.querySelector('.password').value
-   let valdacion =/^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/
+    let valdacion = /^[a-zA-Z0-9]*$/
     if(usuario === '' || password === ''){
         alert('Por favor llene todos los campos')
-    }if(valdacion.test(usuario)){
+    }if(!valdacion.test(password)){
             alert('El usuario es correcto')
+        }else if(!valdacion.test(usuario)  ){
+            alert('el nombre usuario es correcto')
         }else{
             alert('El usuario no es correcto')
         }
